66f7e31cc1b3473bf9a2a92307061a67cc484cc9
Sho Ando <spdtas0223@gmail.com>
2019-08-01 04:19:20 +0800

    no message

---
+++ MessageKit2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessageKit2
+//
+//  Created by 安藤奨 on 2019/08/01.
+//  Copyright © 2019 安藤奨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
